e2de2c1d182acc226ed9a77536baeafcaa86d010
wenchangliu <wenchangliu@google.com>
2022-03-09 11:57:43 +0000

    libvendorgraphicbuffer: fix address offset of ROI metadata
    "SZ_4k * 2" is not correct address offset from hidl_common/Allocator.cpp
    use same address offset as defined in hidl_common/Allocator.cpp
    
    Bug: 188769378
    Test: ROI encoding
    Test: CtsMediaV2TestCases
    Change-Id: I358622a23f863d69eba0a4dafeded1e2703540e2

--- libvendorgraphicbuffer/gralloc4/vendor_graphicbuffer_meta.cpp
+++ libvendorgraphicbuffer/gralloc4/vendor_graphicbuffer_meta.cpp
@@ -24,6 +24,7 @@
 #include "mali_gralloc_buffer.h"
 #include "mali_gralloc_formats.h"
 #include "hidl_common/SharedMetadata.h"
+#include "hidl_common/SharedMetadata_struct.h"
 #include "exynos_format.h"
 
 using namespace android;
@@ -253,7 +254,8 @@ void* VendorGraphicBufferMeta::get_video_metadata_roiinfo(buffer_handle_t hnd)
 		return nullptr;
 
 	if (gralloc_hnd->get_usage() & VendorGraphicBufferUsage::ROIINFO)
-		return static_cast<char*>(gralloc_hnd->attr_base) + SZ_4k * 2;
+		return static_cast<char*>(gralloc_hnd->attr_base) +
+			sizeof(shared_metadata) + gralloc_hnd->reserved_region_size;
 
 	return nullptr;
 }
